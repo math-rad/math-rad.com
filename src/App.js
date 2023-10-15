1b2b38558307c479c5008a9b2c58331c379c1d79
math-rad <59669511+math-rad@users.noreply.github.com>
2023-10-14 22:40:14 -0500

    inital page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <div className="App">
+        <header className="App-header">
+          <body>
+            <h1>math.rad</h1>
+            <body className="alter" >
+              <p>
+                Hello! You've reached my website! Unfortunately, it is currently under development and I do not know when I will finish it. I do have plans and dreams for it, so one day it will be in use. I just wanted to "grab" this domain since I liked it.
+              </p>
+              <p>
+                I chose math.rad rad because it represents how much I love math(it's pretty rad), and my passion for computer science by referencing a math library and indexing the rad(radians) method.
+              </p>
+
+              <p>
+                Thank you for reading! One day this website will be in good use :)
+              </p>
+
+            </body>
+            <body>
+
+            </body>
+
+          </body>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </html>
+
   );
 }
 
