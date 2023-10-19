0919e937b9645d9d3cd4a738b3ca9b66b6203a0b
math-rad <59669511+math-rad@users.noreply.github.com>
2023-10-18 20:18:08 -0500

    root

--- src/App.js
+++ src/App.js
@@ -6,73 +6,15 @@ Description: JS(react) for root domain.
 import logo from './logo.svg';
 import './App.css';
 
-async function upload() {
-  const name = prompt("What name would you like to save the file under?")
-  var input = document.createElement('input');
-  var file
-  input.type = 'file';
-  
-  input.onchange = e => {
-    file = e.target.files[0]
-    input.remove()
 
-    const formData = new FormData()
-    formData.append('file', file)
-    formData.append('name', name)
-
-    fetch("https://api.math-rad.com/uploadv1", {
-      method: 'post',
-      body: formData,
-    })
-      .then(response => response.text())
-      .then(data => {
-        console.log(data); // Access the response data here
-      })
-      .catch(error => {
-        console.error(error); // Handle any errors
-      });
-
-
-  }
-
-  input.click();
-
-}
-
- function download() {
-  const name = prompt("What file do you want to download?");
-  const formData = new FormData();
-  formData.append("name", name);
-
-  fetch("https://api.math-rad.com/downloadv1", {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => response.blob() )
-    .then(blob => {
-      const url = URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = name;
-      link.click();
-    })
-    .catch(error => {
-      console.error(error); // Handle any errors
-    });
-}
-console.log('lastest')
-
-
-function App() {
+function App(req) {
+  const path = window.location.
   return (
     <html>
       <div className="App">
         <header className="App-header">
           <body>
             <h1>math.rad</h1>
-
-            <button onClick={upload}>upload file</button>
-            <button onClick={download}>download file</button>
             <body className="alter" >
               <p>
                 Hello! You've reached my website! Unfortunately, it is currently under development and I do not know when I will finish it. I do have plans and dreams for it, so one day it will be in use. I just wanted to "grab" this domain since I liked it.
@@ -84,10 +26,8 @@ function App() {
               <p>
                 Thank you for reading! One day this website will be in good use :)
               </p>
-
             </body>
             <body>
-
             </body>
 
           </body>
