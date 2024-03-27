d9b0e2d44ba69a43004187a4335d96007783e646
math.rad <ben.sir545@outlook.com>
2024-03-26 22:55:37 -0500

    fix cdn

--- cdn/cdn.js
+++ cdn/cdn.js
@@ -4,6 +4,9 @@ const errorStrings = require("./errorStrings.json")
 const ports = require("../ports.json")
 const cdn = express()
 
+const fileROOT = `${__dirname}/file/`
+const indexROOT = `${__dirname}/indexes/`
+
 const stringify = JSON.stringify
 
 function updateFileIndex() {
@@ -11,14 +14,19 @@ function updateFileIndex() {
 }
 
 cdn.get("/*", (request, response) => {
-    const components = request.url.substring(1).split('/')
+    const components = request.path.substring(1).split('/')
+    console.log(components[0])
+   
     switch(components[0]) {
         case "file": {
-            const path = `./files/${components[1]}`
+            const path = `./file/${components[1]}`
             if (!fs.existsSync(path)) {
-                response.redirect(`error?e=invalidfile&c=${components[1]}`)
+                response.redirect(`/error?e=invalidfile&c=${components[1]}&url=${request.url}`)
                 break
             }
+            response.sendFile(components[1], {
+                "root": fileROOT
+            });
             break 
         }
 
@@ -26,40 +34,37 @@ cdn.get("/*", (request, response) => {
             const index = components[1]
             const path = `./indexes/${index}.json`
             if (!fs.existsSync(path)) {
-                response.redirect(`error?e=badindex&c=${index}`)
+                response.redirect(`/error?e=badindex&c=${index}&url=${request.url}`)
                 break
             }
             const fileInfo = require(path)
             response.sendFile(fileInfo.path, {
-                "root": `${__dirname}/files/`
+                "root": `${__dirname}/file/`
             })
             break
         }
 
         case "error": {
+            console.log("i got an error!")
             const errorIndex = request.query.e
             const errorInfo = errorStrings[errorIndex]
-            console.log(errorIndex)
-            console.log(errorInfo)
             if (!errorInfo) {
-                request.redirect(`error?e=invaliderror&c=${errorIndex}`)
+                request.redirect(`/error?e=invaliderror&c=${errorIndex}`)
                 break
             }
-
             response.status(errorInfo.code).send(
-                `error: <b>${errorInfo.code}</b>\n,
-                ${errorInfo.content}\n
-                ${errorInfo.suggestion && errorInfo.suggestion || ''}\n
-                ${request.query.c && `regarding "${request.query.c}"`}\n
-                ${request.query.url && `original url: ${request.query.url}`}
-                `
+                `error: <b>${errorInfo.code}</b>
+                ${errorInfo.content && `<p>${errorInfo.content}</p>`}
+                ${errorInfo.suggestion && `<p>${errorInfo.suggestion}</p>` || ''}
+                ${request.query.c && `<p>regarding <q>${request.query.c}</q> from <q>${request.query.url}</q> </p>`}`
             )
 
             break
         }
 
         default: {
-            response.redirect(`error?e=path&c=${components[0]}&url=${request.originalUrl}`)
+            response.redirect(`/error?e=path&c=${components[0]}&url=${request.originalUrl}`)
+            break
         }
     }
 })
