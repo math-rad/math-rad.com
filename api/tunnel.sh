380c6f50cc90611e42d70c60a4cb360ffed8b72d
math.rad <ben.sir545@outlook.com>
2024-03-27 01:13:37 -0500

    tunnel

---
+++ api/tunnel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 3/27/2024 12:51AM; universal tunnel script as an alternative to port forwarding
+
+subdomain = ${$(pwd)##*/}
+
+local_port = $(jq ".$subdomain" ../ports.json)
+domain_port = jq ".domainport" ../ports.json
+host = jq ".localhost" ../ports.json
+proxy = jq ".proxy" ../ports.json
+
+domain = "$subdomain.math-rad.com" 
+
+$(ssh $domain:$domain_port:$host.$local_port $proxy)
