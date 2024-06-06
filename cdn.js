fe3800bdfe1c6f227330f69c02baab603319f00c
ben <ben.sir545@outlook.com>
2024-06-06 10:04:59 -0500

    remove random character that I don't know how or where it came from

--- cdn.js
+++ cdn.js
@@ -15,7 +15,6 @@ function serve() {
 cdn.get("/internal/*", (request, response) => {
     const index = request.path.substring(1).split('/')[1]
     const path = `${__dirname}/${internalIndex[index]}`
-A
     if (fs.existsSync(path)) {
         response.sendFile(path)
         console.log(path)
