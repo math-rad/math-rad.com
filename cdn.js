f5325df1d9ede96385cf85bff1e8f440d5a50823
ben <ben.sir545@outlook.com>
2024-06-06 09:40:36 -0500

    cdn + nginx test

---
+++ cdn.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const fs = require("fs")
+
+const ports = require("./ports.json")
+const configurations = require("./configurations.json")
+
+const internalIndex = require("./cdn/internal/index.json")
+
+const cdn = express()
+
+function serve() {
+
+}
+
+cdn.get("/internal/*", (request, response) => {
+    const index = request.path.substring(1).split('/')[1]
+    const path = `${__dirname}/${internalIndex[index]}`
+
+    if (fs.existsSync(path)) {
+        response.sendFile(path)
+        console.log(path)
+    }
+})
+
+cdn.get("/error", (request, response) => {
+
+})
+
+cdn.listen(ports.cdn)
